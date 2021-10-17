d98b2c1d0d1d3b3dd4c876b5ea7770e863c856f8
KadomtsevVolodymyr <volodymyr.kadomtsev@gmail.com>
2021-10-17 23:49:52 +0300

    first commit

--- index.js
+++ index.js
@@ -12,36 +12,35 @@ bot.on('message', (msg) => {
       inline_keyboard: keyboardStart,
     },
   });
-	bot.editMessageReplyMarkup({ reply_markup: JSON.stringify({ keyboard: [] })})
 });
 
 const keyboardMenu = [
-	[
-		{
-			text: '⬆️Главное меню⬆️', // текст на кнопке
+  [
+    {
+      text: '⬆️Главное меню⬆️', // текст на кнопке
       callback_data: 'homeMenu', // данные для обработчика событий
-		},
-	]
-]
+    },
+  ],
+];
 
 const keyboardStart = [
-	[
-		{
-			text: '🗓Расписание пар🗓', // текст на кнопке
+  [
+    {
+      text: '🗓Расписание пар🗓', // текст на кнопке
       callback_data: 'lessons', // данные для обработчика событий
-		},
-	],
-	[
-	{
-		text: '⏰Расписание звонков⏰', // текст на кнопке
-		callback_data: 'time', // данные для обработчика событий
-	},
-]
-]
+    },
+  ],
+  [
+    {
+      text: '⏰Расписание звонков⏰', // текст на кнопке
+      callback_data: 'time', // данные для обработчика событий
+    },
+  ],
+];
 
 const keyboard = [
   [
-		{
+    {
       text: 'Понедельник', // текст на кнопке
       callback_data: 'monday', // данные для обработчика событий
     },
@@ -51,7 +50,7 @@ const keyboard = [
     },
   ],
   [
-		{
+    {
       text: 'Среда', // текст на кнопке
       callback_data: 'wednesday', // данные для обработчика событий
     },
@@ -61,7 +60,7 @@ const keyboard = [
     },
   ],
   [
-		{
+    {
       text: 'Пятница',
       callback_data: 'friday',
     },
@@ -70,62 +69,72 @@ const keyboard = [
       callback_data: 'saturday', //внешняя ссылка
     },
   ],
-	[
-		{
-			text: '⬆️Главное меню⬆️', // текст на кнопке
+  [
+    {
+      text: '⬆️Главное меню⬆️', // текст на кнопке
       callback_data: 'homeMenu', // данные для обработчика событий
-		},
+    },
   ],
 ];
 
 bot.on('callback_query', (query) => {
+  //const chatId = msg.chat.id;
+  const chatId = query.message.chat.id;
 
-//const chatId = msg.chat.id;
-	const chatId = query.message.chat.id;
+  if (query.data === 'homeMenu') {
+    bot.sendPhoto(chatId, 'photo.jpg', {
+      reply_markup: {
+        inline_keyboard: keyboardStart,
+      },
+    });
+  }
 
-	if (query.data === 'homeMenu') {
-		bot.sendMessage(chatId, 'Хорошего пользования ботом!');
-		bot.sendPhoto(chatId, 'photo.jpg', {
-			reply_markup: {
-				inline_keyboard: keyboardStart,
-			},
-		});
-}
+  if (query.data === 'lessons') {
+    bot.sendMessage(chatId, 'Выбирай день недели!', {
+      reply_markup: { inline_keyboard: keyboard },
+    });
+  }
 
-	if (query.data === 'lessons') {
-		bot.sendMessage(chatId, 'Выбирай день недели!',
-			{reply_markup: {inline_keyboard: keyboard}});
-}
+  if (query.data === 'time') {
+    bot.sendPhoto(chatId, 'time.jpg', {
+      reply_markup: { inline_keyboard: keyboardMenu },
+    }); //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+  }
 
-if (query.data === 'time') {
-	bot.sendPhoto(chatId, 'time.jpg',
-		{reply_markup: {inline_keyboard: keyboardMenu}});//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
-}
+  if (query.data === 'monday') {
+    bot.sendMessage(chatId, 'В этот день в вашей группы нет занятий!', {
+      reply_markup: { inline_keyboard: keyboard },
+    });
+  }
 
-if (query.data === 'monday') {
-	bot.sendMessage(chatId, 'В этот день в вашей группы нет занятий!',
-		{reply_markup: {inline_keyboard: keyboard}});
-}
+  if (query.data === 'tuesday') {
+    bot.sendPhoto(chatId, 'tuesday.jpg', {
+      reply_markup: { inline_keyboard: keyboard },
+    });
+  }
 
-	if (query.data === 'tuesday') {
-		bot.sendPhoto(chatId, 'tuesday.jpg',
-			{reply_markup: {inline_keyboard: keyboard}});
-}
-
-	if (query.data === 'wednesday') {
-		bot.sendPhoto(chatId, 'wensday.jpg',//Замена на фото
-		{reply_markup: {inline_keyboard: keyboard}});
-}
-	if (query.data === 'thursday') {
-		bot.sendPhoto(chatId, 'thursday.jpg',
-		{reply_markup: {inline_keyboard: keyboard}});
-}
-	if (query.data === 'friday') {
-		bot.sendPhoto(chatId, 'friday.jpg',
-		{reply_markup: {inline_keyboard: keyboard}});
-}
-	if (query.data === 'saturday') {
-		bot.sendPhoto(chatId, 'saturday.jpg',//Замена на фото
-			{reply_markup: {inline_keyboard: keyboard}});
-	}
+  if (query.data === 'wednesday') {
+    bot.sendPhoto(
+      chatId,
+      'wensday.jpg', //Замена на фото
+      { reply_markup: { inline_keyboard: keyboard } }
+    );
+  }
+  if (query.data === 'thursday') {
+    bot.sendPhoto(chatId, 'thursday.jpg', {
+      reply_markup: { inline_keyboard: keyboard },
+    });
+  }
+  if (query.data === 'friday') {
+    bot.sendPhoto(chatId, 'friday.jpg', {
+      reply_markup: { inline_keyboard: keyboard },
+    });
+  }
+  if (query.data === 'saturday') {
+    bot.sendPhoto(
+      chatId,
+      'saturday.jpg', //Замена на фото
+      { reply_markup: { inline_keyboard: keyboard } }
+    );
+  }
 });
